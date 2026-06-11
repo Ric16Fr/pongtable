b606a01c9b1971964277c12dd6179fe55a1ed6c8
Ric16Fr <ri.franke@proton.me>
2026-06-11 23:57:32 +0200

    Pint gefixt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
